19bcac75c34d8293b8a4b5c8928e37d21e0a3d78
KatePrish <prishchepa@sfedu.ru>
2023-12-06 22:31:10 +0300

    Алгоритм вычисления чисел Фибоначчи.

---
+++ main.py
@@ -0,0 +1,10 @@
+def fibonacci(num):
+    x1, x2 = 1, 1
+    arr_fib = []
+    while num > 0:
+        arr_fib.append(x1)
+        x1, x2 = x2, x1 + x2
+        num -= 1
+    print(arr_fib)
+
+fibonacci(15)
