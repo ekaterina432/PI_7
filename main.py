0987e8c38e3ab148c01b57787bd726a8b791ef92
KatePrish <prishchepa@sfedu.ru>
2023-12-07 20:47:35 +0300

    Исправление ошибки

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def fibonacci(num):
     x1, x2 = 1, 1
     arr_fib = []
     while num > 0:
-        arr_fib.appen(x1)
+        arr_fib.append(x1)
         x1, x2 = x2, x1 + x2
         num -= 1
     return(arr_fib)
